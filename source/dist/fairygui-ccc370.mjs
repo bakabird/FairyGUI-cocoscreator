ee48528a925936cd0fc2dde72ba640ac0d27ff86
bakabird <bakabird@sina.cn>
2023-03-13 15:46:20 +0800

    + v1.0.5 修复 spine 显示的一个报错

--- source/dist/fairygui-ccc370.mjs
+++ source/dist/fairygui-ccc370.mjs
@@ -12551,7 +12551,7 @@ class GLoader3D extends GObject {
         else
             this._content.clearTrack(0);
         let skin = this._skinName || this._content.skeletonData.getRuntimeData().skins[0].name;
-        if (this._content["_skeleton"].skin.name != skin)
+        if (!this._content["_skeleton"].skin || this._content["_skeleton"].skin.name != skin)
             this._content.setSkin(skin);
     }
     onChangeDragonBones() {
